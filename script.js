5c618f4806574baa63602e0ed6b940c016a64719
LeilanNaeimi <leilan.n@gmail.com>
2023-12-14 13:06:50 +0300

    add slider

---
+++ script.js
@@ -0,0 +1,26 @@
+const value = document.querySelector("#value");
+const input = document.querySelector("#vol");
+
+let container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
+
+// /******** */
+for (var i = 1; i <= 16; i++) {
+  let row = document.createElement("div");
+  row.style.display = "flex";
+
+  for (var j = 1; j <= 16; j++) {
+    let div = document.createElement("div");
+    div.className = "box";
+    row.appendChild(div);
+  }
+
+  container.appendChild(row);
+}
+/******** slider value ************/
+value.textContent = "16 * 16";
+//  `${input.value} * ${input.value}`;
+input.addEventListener("input", (e) => {
+  value.textContent = `${e.target.value} * ${e.target.value}`;
+});
