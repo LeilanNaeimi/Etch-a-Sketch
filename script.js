9850d0df4007fec243b01ef67ef8cd1255236846
LeilanNaeimi <leilan.n@gmail.com>
2023-12-18 11:35:49 +0300

    fixed container size

--- script.js
+++ script.js
@@ -1,72 +1,36 @@
-let container = document.querySelector('.container');
-let coloredGrid = [];
+const container = document.getElementById('container');
+let gridSize = 16;
 
-function createGrid(numberOfSquares) {
-  //Remove existing grid
+function createGrid() {
   container.innerHTML = '';
+  container.style.setProperty('--gridSize', gridSize);
 
-  //Generate a new grid with selected size
-
-  const maxContainerSizePx = 640;
-  container.style.width = maxContainerSizePx;
-  container.style.hight = maxContainerSizePx;
-
-  const boxSize = maxContainerSizePx / numberOfSquares;
-
-  //Create square divs using a loop
-
-  for (var i = 1; i <= numberOfSquares; i++) {
-    let rows = document.createElement('div');
-    rows.style.display = 'flex';
-
-    for (var j = 1; j <= numberOfSquares; j++) {
-      let pixel = document.createElement('div');
-      pixel.className = 'pixel';
-      rows.appendChild(pixel);
-    }
-    container.appendChild(rows);
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixel.addEventListener('mouseover', function () {
+      setColor(pixel);
+    });
+    container.appendChild(pixel);
   }
-
-  // Apply selected size on container
-
-  container.querySelectorAll('.pixel').forEach((pixel) => {
-    pixel.style.width = `${boxSize}px`;
-    pixel.style.height = `${boxSize}px`;
-  });
 }
 
-function coloring(color) {
-  createGrid(20);
-
-  container.addEventListener('mouseover', changeColor(color));
-
-  function changeColor(newColor) {
-    return function () {
-      container.style.backgroundColor = newColor;
-      coloredGrid.push(container);
-    };
-  }
-  // container.addEventListener('mouseover', setColor);
-
-  // function setColor() {
-  //   let pixels = container.querySelectorAll('.pixel');
-  //   let pixel = container.querySelector('.pixel');
-
-  //   // pixels.forEach((pixel) => {
-  //   pixel.style.setProperty('--hover-color', color);
-  //   container.style.backgroundColor = color;
-  //   coloredGrid.push(container);
-
-  //   console.log(coloredGrid);
-  // });
-  // }
+function setColor(pixel) {
+  pixel.style.backgroundColor = 'gray';
 }
 
-// function btnClick() {
-//   console.log('btnClick');
-//   //Display a prompt to get user input for the number of squares per side
+function changeGridSize() {
+  let newSize = prompt('Enter the number of squares (max 100):');
 
-//   //Call the grid generation function with the entered input
-// }
+  newSize = parseInt(newSize);
+  // console.log(newSize);
+  if (isNaN(newSize) || newSize < 1) {
+    alert('Please enter a valid number greater than 0');
+    return;
+  }
 
-coloring('orange');
+  gridSize = Math.min(newSize, 100);
+  createGrid();
+}
+
+createGrid();
